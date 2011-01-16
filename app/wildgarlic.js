652fe3d2aaf3709fc301475bd6a927c2f03adf9d
Ed Finkler <coj@funkatron.com>
2011-01-16 17:46:56 -0500

    lots and lots of reworking

--- app/wildgarlic.js
+++ app/wildgarlic.js
@@ -9,53 +9,118 @@ $L = function(str) {
 	return str;
 };
 
+// init jo
+jo.load();
+
+
+jo.setDebug(true);
 
 
 var WildGarlic = function() {
 	
 	var self = this;
-
-	self.searchResults = [];
+	
+	self.model = new joRecord({
+		searchResults  : [],
+		selectedResult : null,
+		searchText     : ''
+	});
+	
 	self.ui = {};
-
+	
 
 	/**
 	 * setup & load the app UI 
 	 */
 	function load() {
 		
-		// init jo
-		jo.load();
+		
+		
+		joDefer(function() {
+			// dynamic CSS loading based on platform, in this case FireFox
+			// doesn't do stack transitions well, so we're downshifting
+
+			if (jo.matchPlatform("iphone ipad safari")) {
+				joDOM.loadCSS("./jo/css/aluminum/webkit.css");
+			} else if (jo.matchPlatform("chrome webkit webos")) {
+				joDOM.loadCSS("./jo/css/aluminum/webkit.css");
+				joDOM.loadCSS("./jo/css/aluminum/chrome.css");
+			} else {
+				joDOM.loadCSS("./jo/css/aluminum/gecko.css");
+			}
+			// as an optimization, I recommend in a downloadable app that
+			// you create a custom CSS file for each platform using some
+			// sort of make-like process.
+		}, this);
+		
+		
 		
 		// if webOS, tell it the stage is ready
 		if (window.PalmSystem) {
 		   window.PalmSystem.stageReady();
 		}
 		
-		
-		// make a new screen
-		self.ui.stack = new joStackScroller();
-		self.ui.scn = new joScreen(self.ui.stack);
 
+		
+		// make a new screen		
+		self.ui.screen = new joScreen(
+			new joContainer([
+				new joFlexcol([
+					self.ui.nav   = new joNavbar(),
+					self.ui.stack = new joStackScroller()
+				]),
+				self.ui.toolbar = new joToolbar("This is a footer, neat huh?")
+			]).setStyle({position: "absolute", top: "0", left: "0", bottom: "0", right: "0"})
+		);
+		self.ui.nav.setStack(self.ui.stack);
+		
+		
+		
 		/**
-		 * make UI elements 
+		 * make cards 
 		 */
-		self.ui.title = new joTitle($L('WildGarlic'));
-		self.ui.searchBox = new joInput();
-		self.ui.searchButton = new joButton($L('Search')).selectEvent.subscribe(search, self);
+		self.ui.cards = {
+			
+			home:homeCard(),
+			def :defCard() 
+			
+		};
+		
+		
+		
+		joGesture.forwardEvent.subscribe(self.ui.stack.forward, self.ui.stack);
+		joGesture.backEvent.subscribe(self.ui.stack.pop, self.ui.stack);
+		
+		
+		
+		/**
+		 * push initial card to stack 
+		 */
+		self.ui.stack.push(self.ui.cards.home);
+		
+		
+	};
+
+	
+	
+	/**
+	 * set up the home card 
+	 */
+	function homeCard() {
+		
 		
 		/**
 		 * setup results list 
 		 */
-		self.ui.resultsList = new joList(self.searchResults).attach(document.body);
+		var resultsList = new joList(self.model.link('searchResults'));
 		// formatItem is your renderer
-		self.ui.resultsList.formatItem = function(data, index) {
+		resultsList.formatItem = function(data, index) {
 			var title = data.word + " (" + data.thumbs_up + ")";
 			// call the prototype version so we get the cool formatting
 			return joList.prototype.formatItem.call(this, title, index);
 		};
 		// comparison method
-		self.ui.resultsList.compareItems = function(a,b) {
+		resultsList.compareItems = function(a,b) {
 			if (a.thumbsUp > b.thumbsUp)
 			    return 1;
 			else if (a.thumbsUp == b.thumbsUp)
@@ -63,26 +128,41 @@ var WildGarlic = function() {
 			else
 			    return -1;
 		};
-		self.ui.resultsList.selectEvent.subscribe(onItemSelect, self);
+		resultsList.selectEvent.subscribe(onItemSelect, self);
 		
 		
 		/**
 		 * create the card 
 		 */
-		self.ui.card = new joCard([
-			self.ui.title,
-			self.ui.searchBox,
-			self.ui.searchButton,
-			self.ui.resultsList
-		]);
+		var card = new joCard([
+			new joInput(self.model.link('searchText')),
+			new joButton($L('Search')).selectEvent.subscribe(search, self),
+			resultsList
+		]).setTitle($L('WildGarlic'));
 
-
-		/**
-		 * push card to stack 
-		 */
-		self.ui.stack.push(self.ui.card);
+		return card;
+	}
+	
+	
+	/**
+	 * set up the definition card 
+	 */
+	function defCard() {
 		
-	};
+		/**
+		 * create the card 
+		 */
+		var card = new joCard([
+			new joCaption("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus nisl dolor, pulvinar id, pharetra a, egestas nec, ante. Duis scelerisque eleifend metus. Sed non odio id odio varius rutrum. Pellentesque congue commodo lacus. In semper pede lacinia felis. Morbi mollis molestie lorem. Morbi suscipit libero. Quisque ut erat sit amet elit aliquam nonummy. Donec tortor. Aliquam gravida ullamcorper pede. Praesent eros. Sed fringilla ligula sed odio pharetra imperdiet. Integer aliquet quam vitae nibh. Nam pretium, neque non congue vulputate, odio odio vehicula augue, sit amet gravida pede massa ac lectus. Curabitur a libero vitae dui sagittis aliquet. Ut suscipit. Curabitur accumsan sem a urna. Ut elit pede, vulputate sed, feugiat quis, congue sed, lacus.")
+		]).setTitle('Definition');
+		
+		card.activate = function() {
+			this.setTitle(self.model.getProperty('selectedResult').word);
+		};
+
+		return card;
+		
+	}
 	
 	
 	
@@ -90,17 +170,14 @@ var WildGarlic = function() {
 	 * search for results 
 	 */
 	function search() {
-		joFocus.set(self.ui.searchBox);
-		var searchtxt = self.ui.searchBox.getData();
-		if (searchtxt) {
+		var searchText = self.model.getProperty('searchText');
+		if (searchText) {
 			joFile(
-				'http://www.urbandictionary.com/iphone/search/define?term='+window.encodeURIComponent(searchtxt),
+				'http://www.urbandictionary.com/iphone/search/define?term='+window.encodeURIComponent(searchText),
 				function(data, context, error) {
 					if (data) {
 						var result = JSON.parse(data);
-						self.searchResults = result.list;
-						self.ui.resultsList.data = self.searchResults;
-						self.ui.resultsList.refresh();
+						self.model.setProperty('searchResults', result.list);
 					}
 				},
 				self
@@ -113,7 +190,9 @@ var WildGarlic = function() {
 	 * handle taps on the search items 
 	 */
 	function onItemSelect(index) {
-		self.ui.scn.alert(self.ui.resultsList.data[index].definition);
+		var results = self.model.getProperty('searchResults');
+		self.model.setProperty('selectedResult', results[index]);
+		self.ui.stack.push(self.ui.cards.def);
 	}
 	
 	
